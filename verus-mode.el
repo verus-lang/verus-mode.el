182d003a45db12b55c1fc6986919dbb1ab971220
Jay Bosamiya <jaybosamiya@gmail.com>
2024-02-12 14:43:02 -0500

    automatic path shifts if necessary for extra_args

--- verus-mode.el
+++ verus-mode.el
@@ -333,11 +333,10 @@ This is done by checking if the file contains a `fn main` function."
                   main
                 (error "Could not find crate root file")))))))))
 
-(defun verus--extra-args-from-cargo-toml ()
-  "Return a list of extra arguments to pass to Verus.
+(defun verus--extra-args-from-cargo-toml--direct ()
+  "The args from the Cargo.toml, directly, without modification.
 
-This reads the `extra_args` key from the `package.metadata.verus.ide`
-table in the Cargo.toml for the current crate."
+You probably instead want `verus--extra-args-from-cargo-toml'."
   (let ((root (locate-dominating-file default-directory "Cargo.toml")))
     (when root
       (let* ((toml (f-join root "Cargo.toml"))
@@ -367,6 +366,37 @@ table in the Cargo.toml for the current crate."
                         (buffer-substring start end))))))))
         (when extra-args
           (split-string-and-unquote (string-trim extra-args)))))))
+
+(defun verus--path-shift-relative (path old new)
+  "Shift relative PATH from OLD to NEW."
+  (f-relative (f-join old path) new))
+
+(defun verus--extra-args-from-cargo-toml ()
+  "Return a list of extra arguments to pass to Verus.
+
+This reads the `extra_args` key from the
+`package.metadata.verus.ide` table in the Cargo.toml for the
+current crate. It additionally updates any paths found to be
+relative to the current working directory (while the original
+ones are relative to the Cargo.toml)."
+  (let ((root (locate-dominating-file default-directory "Cargo.toml"))
+        (args (verus--extra-args-from-cargo-toml--direct))
+        (cwd (f-full default-directory)))
+    (when args
+      (mapcar (lambda (arg)
+                (if (string-match "=" arg)
+                    (let ((lhs (substring arg 0 (match-beginning 0)))
+                          (rhs (substring arg (match-end 0))))
+                      (message "lhs: %s, rhs: %s" lhs rhs)
+                      (if (or (string-prefix-p "./" rhs)
+                              (string-prefix-p "../" rhs))
+                          (concat lhs "=" (verus--path-shift-relative rhs root cwd))
+                        arg))
+                  (if (or (string-prefix-p "./" arg)
+                          (string-prefix-p "../" arg))
+                      (verus--path-shift-relative arg root cwd)
+                    arg)))
+              args))))
 
 (defun verus--run-on-crate-command ()
   "Return the command to run Verus on the current crate.
